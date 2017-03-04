91ab798a726a747626303f302c5b1daf063065f9
olwj <al.wajgert@gmail.com>
2017-03-04 13:01:02 +0100

    add final files

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double a = 100;
+        double b = 1.80;
+
+        double bmi = a/b*b;
+        System.out.println("bmi "+ bmi);
+
+        if (bmi > 24.9) {
+            System.out.println
+        }
+
+
+
+
+        System.out.println("!");
+    }
+}
